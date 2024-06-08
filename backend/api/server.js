650683774f866e66b6556cf64946297615b1823a
Hrithik Keswani <hrithikkeswani200@gmail.com>
2024-06-08 18:03:45 +0530

    Configure project for Vercel deployment

---
+++ backend/api/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+const tokensRoutes = require('./routes/tokensRoutes');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/api', tokensRoutes);
+
+module.exports = app;
